52b647e389df8538e3b8d352252d2588df1f9696
Swatts96 <w0292233@nscc.ca>
2023-12-10 12:46:23 -0400

    reminder comments

--- script.js
+++ script.js
@@ -7,6 +7,7 @@
 
 
  // This part ensures that the script runs after the HTML document has been completely loaded. 
+ //also defines a function for the entire html, so building within this. (stop adding functions outside) 
 document.addEventListener("DOMContentLoaded", function() {
 
     var dropdown = document.getElementById("countryDropdown");
@@ -14,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var populationDisplay = document.getElementById("populationDisplay");
     var areaDisplay = document.getElementById("areaDisplay");
     var selectedCountryName = document.getElementById("selectedCountryName");
-    
+
     const worldPop = 7888000000;
    
    
