b32521b81c4a17f17efc8a55d2d9d2d772f9a928
Swatts96 <w0292233@nscc.ca>
2023-12-10 10:58:36 -0400

    population reading

--- script.js
+++ script.js
@@ -3,20 +3,21 @@
 //Course: LogProg1700 
 //Instructor: Hamlet Lin
 
-
 document.addEventListener("DOMContentLoaded", function() {  
     // This part ensures that the script runs after the HTML document has been completely loaded. 
     
     var dropdown = document.getElementById("countryDropdown");
     var flagImage = document.getElementById("flagImage");
-   // These lines select HTML elements with the IDs "countryDropdown" and "flagImage", respectively. These elements are presumably a dropdown menu for selecting countries and an image tag for displaying the flag.
-    
+   // These lines select HTML elements respectively. 
+   //- Dropdown menu for selecting countries and an image tag for displaying the flag.
+   //- populating our population container with the "Population" data (sorry)
     // Fetch JSON data from the file
     // This fetches the JSON data from the "countries.json" file. The fetch function returns a promise. The then method is used to handle the result of this asynchronous operation.
     fetch("countries.json")
         .then(response => response.json())
         .then(countries => { 
-            // Once the JSON data is successfully fetched and parsed as an array of country objects, this part creates an option element for each country, sets its value and text properties to the country name, and appends it to the dropdown menu.
+            // Once the JSON data is successfully fetched and parsed as an array of country objects, this part creates an option element for each country, 
+            // sets its value and text properties to the country name, and appends it to the dropdown menu.
             // TLDR Populate the dropdown with country names
             countries.forEach(function(country) {
                 var option = document.createElement("option");
@@ -35,7 +36,9 @@ document.addEventListener("DOMContentLoaded", function() {
             })
               .catch(error => console.error("Error fetching countries.json:", error));
             //Catch error for missing files/mis-path 
-            //This sets up an event listener for the dropdown menu. When a country is selected, it retrieves the selected country, generates the corresponding flag image file name, constructs the file path, and updates the src attribute of the flag image element to display the selected country's flag.
+            //This sets up an event listener for the dropdown menu. When a country is selected, it retrieves the selected country, generates the corresponding flag image file name, 
+            //constructs the file path, and updates the src attribute of the flag image element to display the selected country's flag.
+
                 dropdown.addEventListener("change", function() {
                     var selectedCountry = dropdown.value;
                     //Set the flag image source based on the selected country
@@ -47,5 +50,5 @@ document.addEventListener("DOMContentLoaded", function() {
             })
 
 
-            
+            const worldPop = 7888000000;
 // add function for determining world pop % (parseint(Country pop / 7,888,000,0000).tofixed(2))
