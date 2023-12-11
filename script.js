44ad2102541775c7bde245f92dbf4b8b343f212d
Swatts96 <w0292233@nscc.ca>
2023-12-10 20:00:13 -0400

    Fixed calcworldpop function + ternary expr

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 //Date: Dec 2023
 //Course: LogProg1700 
 //Instructor: Hamlet Lin
+
  // This part ensures that the script runs after the HTML document has been completely loaded. 
  //also defines a function for the entire html, so building within this. (stop adding functions outside) 
 
 document.addEventListener("DOMContentLoaded", function() {
 
-    var dropdown = document.getElementById("countryDropdown");
+    var countryDropdown = document.getElementById("countryDropdown");
     var flagImage = document.getElementById("flagImage");
     var selectedCountryName = document.getElementById("selectedCountryName");
     var populationDisplay = document.getElementById("populationValue");
@@ -15,9 +16,15 @@ document.addEventListener("DOMContentLoaded", function() {
     var areaDisplay = document.getElementById("areaValue");
 
     var countries; // Variable to store the JSON data
-    const worldPopulation = 7888888888; // Variable to store world population
-   
-    fetch("countries.json") //fetch api retrieving contents of our countries.json file
+
+ // Fetch API comments
+    // These lines select HTML elements respectively. 
+    //- Dropdown menu for selecting countries and an image tag for displaying the flag.
+    //- populating our population container with the "Population" data (sorry)
+    // Fetch JSON data from the file
+    // This fetches the JSON data from the "countries.json" file. The fetch function returns a promise. The then method is used to handle the result of this asynchronous operation.
+
+    fetch("countries.json") 
         .then(response => response.json())
         .then(countriesData => {
             countries = countriesData; // 3 hours troubleshooting for this one missing line 
@@ -28,22 +35,22 @@ document.addEventListener("DOMContentLoaded", function() {
                 var option = document.createElement("option");
                 option.value = country.Name;
                 option.text = country.Name;
-                dropdown.appendChild(option);
+                countryDropdown.appendChild(option);
             });
             // Default Country/Home Country (Assigning a home page)
             // set variable for our defaultCountryIndex that will be used to retrieve default flag instead of blank 
             var defaultCountryIndex = countries.findIndex(country => country.Name === "Canada");
-            dropdown.selectedIndex = defaultCountryIndex;
+            countryDropdown.selectedIndex = defaultCountryIndex;
             // Trigger the change event to load the default flag (event comments)
             // Event being a term we haven't used much, but w3 explains well. 
             // any change, event or anything that happens to an html element upon command, in this case changing the flag countryIndex to Canada
             var event = new Event("change");
-            dropdown.dispatchEvent(event);
+            countryDropdown.dispatchEvent(event);
         })
         .catch(error => console.error("Error fetching countries.json:", error));
     
-    dropdown.addEventListener("change", function() {
-        var selectedCountry = dropdown.value;
+        countryDropdown.addEventListener("change", function() {
+        var selectedCountry = countryDropdown.value;
         //Set the flag image source based on the selected country
         var flagFileName = selectedCountry.charAt(0).toUpperCase() + selectedCountry.slice(1) + ".png"; 
         var flagFilePath = "flags/" + flagFileName;
@@ -53,37 +60,30 @@ document.addEventListener("DOMContentLoaded", function() {
         
         // Find the selected country in the JSON data
         var selectedCountryData = countries.find(country => country.Name === selectedCountry);
+            var worldPopulation = calculateWorldPopulation(countries);
 
         // Update HTML content
         selectedCountryName.textContent = selectedCountry;
+
         populationDisplay.textContent = selectedCountryData ? selectedCountryData.Population : "N/A";
-        worldPopulationPercentageDisplay.textContent = selectedCountryData ? calculateWorldPopulationPercentage(selectedCountryData.Population) : "N/A";
+
+        worldPopulationPercentageDisplay.textContent = selectedCountryData ? calculateWorldPopulationPercentage(selectedCountryData.Population, worldPopulation) : "N/A";
+
         areaDisplay.textContent = selectedCountryData ? selectedCountryData.Area : "N/A";
-   
     });
 })
 
-// Function to calculate world population
-function calculateWorldPopulation(countries) {
+ // Function to calculate world population
+ function calculateWorldPopulation(countries) {
     return countries.reduce((total, country) => total + country.Population, 0);
 }
 
 // Function to calculate world population percentage
-function calculateWorldPopulationPercentage(countryPopulation) {
+function calculateWorldPopulationPercentage(countryPopulation, worldPopulation) {
     return ((countryPopulation / worldPopulation) * 100).toFixed(2) + "%";
 }
 
 
- // Fetch comments
-    // These lines select HTML elements respectively. 
-    //- Dropdown menu for selecting countries and an image tag for displaying the flag.
-    //- populating our population container with the "Population" data (sorry)
-    // Fetch JSON data from the file
-    // This fetches the JSON data from the "countries.json" file. The fetch function returns a promise. The then method is used to handle the result of this asynchronous operation.
-
 //Catch error for missing files/mis-path 
     //This sets up an event listener for the dropdown menu. When a country is selected, it retrieves the selected country, generates the corresponding flag image file name, 
     //constructs the file path, and updates the src attribute of the flag image element to display the selected country's flag.
-
-
-// add function for determining world pop % (parseint(Country pop / 7,888,000,0000).tofixed(2))
